499bc54831df3b2db398450349639e8e9cf4ce21
iegorov99 <egorov.1999.ilya@yandex.ru>
2023-03-18 09:52:50 +0300

    Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int DegreeA(int A, int B)
+{
+    int result = A;
+    for (int i = 1; i < B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
+double DegreeB(int A, int B)
+{
+    double result = Convert.ToDouble(A);
+    B = Math.Abs(B);
+    for (int i = 1; i < B; i++)
+    {
+        result = result * A;
+    }
+    result = 1 / result;
+    return result;
+}
+
+try
+{
+    Console.Write("Введите первое натуральное число число: ");
+    int A = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе натуральное число число: ");
+    int B = Convert.ToInt32(Console.ReadLine());
+    if (A == 0)
+        Console.WriteLine("Некорреектное первое число");
+    else if (B == 0)
+        Console.WriteLine($"Число {A} в степени {B} равно 1");
+    else if (B > 0)
+        Console.WriteLine($"Число {A} в степени {B} равно {DegreeA(A, B)}");
+    else
+        Console.WriteLine($"Число {A} в степени {B} равно {DegreeB(A, B)}");
+}
+catch
+{
+    Console.WriteLine("Одно или нексколько чисел введены некорреектно");
+}
