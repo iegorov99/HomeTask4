dad57921a88f976dc0e6cedf98b8426e086127c2
iegorov99 <egorov.1999.ilya@yandex.ru>
2023-03-18 13:36:46 +0300

    Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int A)
+{
+    int res = 0;
+    while (A > 0)
+    {
+        res += A % 10;
+        A /= 10;
+    }
+    return res;
+}
+
+Console.Write("Введите число:");
+decimal num = Convert.ToDecimal(Console.ReadLine());
+decimal numA = num;
+while (numA % 1 > 0)
+{
+    numA *= 10;
+}
+int A = Convert.ToInt32(numA);
+Console.WriteLine($"Сумма цифр числа {num} равна {Sum(A)}");
